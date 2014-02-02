883e9af481ba050248ebad462b215cd251da3bf2
Maciej Nowak <maciejt.nowak@gmail.com>
2014-02-02 16:34:10 +0100

    little clean up

--- src/CodeModel/Graphs/DepthFirstSearch.cs
+++ src/CodeModel/Graphs/DepthFirstSearch.cs
@@ -8,42 +8,27 @@ namespace CodeModel.Graphs
         private HashSet<Node> visited;
 
         public void Walk(Node startNode)
-        {
-            this.visited = new HashSet<Node>();
-
+        {          
             WalkCore(startNode, Enumerable.Empty<Link>());
         }
 
         private void WalkCore(Node node, IEnumerable<Link> availableThrough)
-        {            
+        {
             var links = availableThrough as IList<Link> ?? availableThrough.ToList();
 
             EnterNode(node, links);
 
-            if (!this.visited.Contains(node))
+            foreach (var target in GetOutboundTargets(node))
             {
-               // this.visited.Add(node);
-
-                foreach (var target in GetOutboundTargets(node))
-                {
-                    WalkCore(target.Key, target);
-                }
-            }
-            else
-            {
-                AlreadyVisited(node, links);
+                WalkCore(target.Key, target);
             }
 
-            LeaveNode(node, links);                   
+            LeaveNode(node, links);
         }
 
         protected virtual IEnumerable<IGrouping<Node, Link>> GetOutboundTargets(Node node)
         {
             return node.OutboundLinks.GroupBy(x => x.Target);
-        }
-
-        protected virtual void AlreadyVisited(Node node, IEnumerable<Link> availableThrough)
-        {            
         }
 
         protected abstract void EnterNode(Node node, IEnumerable<Link> availableThrough);
--- src/CodeModel/Graphs/FindAllPaths.cs
+++ src/CodeModel/Graphs/FindAllPaths.cs
@@ -12,14 +12,12 @@ namespace CodeModel.Graphs
         private readonly Node endNode;
         public List<IEnumerable<Node>> Paths { get; private set; }
         private readonly Stack<Node> currentPath;
-        private readonly List<List<Node>> circularPaths;
 
         protected FindAllPaths(Node endNode)
         {
             this.endNode = endNode;
             this.currentPath = new Stack<Node>();
-            this.Paths = new List<IEnumerable<Node>>();
-            this.circularPaths = new List<List<Node>>();
+            this.Paths = new List<IEnumerable<Node>>();            
         }
 
         public static IEnumerable<IEnumerable<Node>> BetweenNodes(Node startNode, Node endNode)
@@ -27,46 +25,8 @@ namespace CodeModel.Graphs
             var search = new FindAllPaths(endNode);
 
             search.Walk(startNode);
-
-            //search.ResolveCircularPaths();
-
+          
             return search.Paths;
-        }
-
-        private void ResolveCircularPaths()
-        {
-            while (this.circularPaths.Any())
-            {
-                var currentPaths = this.Paths.ToList();
-
-                int count = this.circularPaths.Count;
-
-                for (int i = this.circularPaths.Count - 1; i >= 0; i--)
-                {
-                    bool resolved = false;
-
-                    var circularPath = circularPaths[i];
-                    var lastNode = circularPath.Last();
-
-                    var possibleEndings = currentPaths.Where(x => x.Contains(lastNode)).Select(x => x.SkipWhile(y => !y.Equals(lastNode))).Distinct(new PathComparer()).ToList();
-
-                    foreach (var possibleEnding in possibleEndings)
-                    {
-                        this.Paths.Add(new List<Node>(circularPath.Union(possibleEnding)));
-                        resolved = true;
-                    }
-
-                    if (resolved)
-                    {
-                        this.circularPaths.RemoveAt(i);
-                    }
-                }
-
-                if (count == this.circularPaths.Count)
-                {
-                    break;
-                }
-            }
         }
 
         protected override void EnterNode(Node node, IEnumerable<Link> availableThrough)
@@ -82,27 +42,6 @@ namespace CodeModel.Graphs
         protected override void LeaveNode(Node node, IEnumerable<Link> availableThrough)
         {
             this.currentPath.Pop();
-        }
-
-        protected override void AlreadyVisited(Node node, IEnumerable<Link> availableThrough)
-        {
-            if (!node.Equals(this.endNode))
-            {
-                this.circularPaths.Add(new List<Node>(this.currentPath.Reverse()));
-            }
-        }
-
-        private class PathComparer : IEqualityComparer<IEnumerable<Node>>
-        {
-            public bool Equals(IEnumerable<Node> x, IEnumerable<Node> y)
-            {
-                return x.Zip(y, (a, b) => x.Equals(y)).All(a => a);
-            }
-
-            public int GetHashCode(IEnumerable<Node> obj)
-            {
-                return obj.Aggregate(1, (a, y) => a * 7 + y.GetHashCode());
-            }
         }
     }
 }
