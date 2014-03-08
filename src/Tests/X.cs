c1eacba5f9103be80b1521435c4744e00993fbb7
Maciej Nowak <maciejt.nowak@gmail.com>
2014-03-08 11:50:29 +0100

    IL based test targets

---
+++ src/Tests/X.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeModel.FlowAnalysis;
+using CodeModel.Graphs;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class X : IHaveGraph
+    {
+        [Test]
+        public void a()
+        {
+            var x = typeof (TestTarget.IL.Test).GetMethod("Kopytko");
+            var cfg = new ControlFlow().AnalyzeMethod(x);
+
+            this.Result = cfg;
+        }
+
+        public Graph Result { get; private set; }
+    }
+}
