afa13df35b86567dc63b673935dfc7096bb66d2d
Maciej Nowak <maciejt.nowak@gmail.com>
2014-07-13 19:14:04 +0200

    Use BaseCfgWalker to determine call parameters

---
+++ src/CodeModel/FlowAnalysis/BinaryOperator.cs
@@ -0,0 +1,19 @@
+namespace CodeModel.FlowAnalysis
+{
+    public enum BinaryOperator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Remainder,
+        GreaterThan,
+        LessThan,
+        Equal,
+        And,
+        ShiftLeft,
+        ShiftRight,
+        Or,
+        Xor
+    }
+}
