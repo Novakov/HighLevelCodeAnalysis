2fb1598cabb3a16d3bc61f1a1b80ef91d292e657
Maciej Nowak <maciejt.nowak@gmail.com>
2014-12-28 17:24:45 +0100

    Sonar analysis

--- src/build.fsx
+++ src/build.fsx
@@ -1,10 +1,21 @@
 #r "packages/FAKE.3.2.0/tools/FakeLib.dll"
 open Fake
+open Fake.OpenCoverHelper
 
 let buildDir = "..\\build" |> FullName
 let binaries = buildDir @@ "binaries"
 let tests = buildDir @@ "tests"
 
+let packagesDir = "packages" |> FullName
+
+let nunitParameters (defaults:NUnitParams) = { defaults with
+                                                ErrorLevel = DontFailBuild
+                                                Framework = "4.0"
+                                                ShowLabels = false
+                                                WorkingDir = tests
+                                                OutputFile = tests @@ "TestResult.xml"
+                                                DisableShadowCopy = true    
+}
 
 Target "Clean" (fun _ ->
     CleanDir buildDir
@@ -25,25 +36,32 @@ Target "Build" (fun _ ->
 Target "Tests" (fun _ ->   
     CreateDir tests
 
-    let setParams (defaults:NUnitParams) = { defaults with
-                                                ErrorLevel = DontFailBuild
-                                                Framework = "4.0"
-                                                ShowLabels = false
-                                                WorkingDir = tests
-                                                OutputFile = tests @@ "TestResult.xml"
-    
-    }
-
     !! (binaries @@ "Tests.dll" )
-    |> NUnit setParams
+    |> NUnit nunitParameters
     |> DoNothing
+)
+
+Target "TestsWithCoverage" (fun _ ->
+    CreateDir tests
+
+    let setParams (defaults:OpenCoverParams) = { defaults with
+                                                    ExePath = FileSystem.findToolInSubPath "OpenCover.Console.exe" packagesDir
+                                                    Output = tests @@ "coverage.xml"
+                                                    Register = RegisterUser
+                                                    TestRunnerExePath = FileSystem.findToolInSubPath "nunit-console.exe" packagesDir
+    }
+    
+    let targetArgs = NUnitCommon.buildNUnitdArgs (nunitParameters NUnitDefaults) !! (binaries @@ "Tests.dll" )
+
+    OpenCover setParams targetArgs
 )
 
 Target "Default" DoNothing
 
 "Clean"
     ==> "Build"
-    ==> "Tests"
+    =?> ("Tests", not (hasBuildParam "Coverage"))
+    =?> ("TestsWithCoverage", hasBuildParam "Coverage")
     ==> "Default"
 
 
